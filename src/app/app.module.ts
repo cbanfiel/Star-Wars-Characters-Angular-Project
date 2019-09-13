72331a26329c2c8e761e210f6c7177707acf4142
Chad Banfield <cbanfiel@emich.edu>
2019-09-13 11:14:05 -0400

    finished project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+
+import { ItemComponent } from './item/item.component';
+import { StarWarsService } from './star-wars.service';
+import { LogService } from './log.service';
+import { HeaderComponent } from './header/header.component';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    ListComponent,
+    ItemComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [StarWarsService, LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
